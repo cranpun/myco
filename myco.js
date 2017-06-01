ca213186bac166d6b797288d135b35ac942226ce
cranpun <mizuno_k@cranpun.sub.jp>
2017-06-01 18:05:34 +0900

    asymc/awaitでsite内のforとdb書き込みまではOK。

--- myco.js
+++ myco.js
@@ -455,6 +455,41 @@ main();
 
 "use strict";
 
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var client = __webpack_require__(6);
 var conf_1 = __webpack_require__(0);
@@ -466,35 +501,82 @@ var Site = (function () {
     }
     Site.prototype.download = function () {
         var _this = this;
-        return new Promise(function (resolve) {
-            client.set("timeout", conf_1.Conf.timeout);
-            var p = client.fetch(_this.site["url"]);
-            var me = _this;
-            var id = 0;
-            p.then(function (result) {
-                conf_1.Conf.procLog("site", "end : " + _this.site.title);
-                result.$("a").each(function (idx) {
-                    try {
-                        var pageurl = result.$(this).url({ invalid: false }).toString();
-                        url.parse(pageurl);
-                        conf_1.Conf.procLog("site", "each : " + pageurl);
-                        // ダウンロード済みなら実行しない
-                        if (pagesdb_1.Pagesdb.noPage(pageurl)) {
-                            pagesdb_1.Pagesdb.putPage(pageurl);
-                            conf_1.Conf.procLog("site", "page start : " + pageurl);
-                            id++;
-                            // let page = new Page(me.site["title"], pageurl, id);
-                            //await page.download();
-                        }
-                    }
-                    catch (e) {
-                        // do nothing。不正なリンクなので無視。
-                    }
-                });
-                conf_1.Conf.procLog("site", "end : each");
-                resolve();
+        return new Promise(function (resolve) { return __awaiter(_this, void 0, void 0, function () {
+            var _this = this;
+            var p, me, id;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        client.set("timeout", conf_1.Conf.timeout);
+                        p = client.fetch(this.site["url"]);
+                        me = this;
+                        id = 0;
+                        return [4 /*yield*/, p.then(function (result) { return __awaiter(_this, void 0, void 0, function () {
+                                var as, i, a, href, pageurl, e_1;
+                                return __generator(this, function (_a) {
+                                    switch (_a.label) {
+                                        case 0:
+                                            conf_1.Conf.procLog("site", "dl : " + this.site.title);
+                                            as = result.$("a");
+                                            i = 0;
+                                            _a.label = 1;
+                                        case 1:
+                                            if (!(i < as.length)) return [3 /*break*/, 8];
+                                            _a.label = 2;
+                                        case 2:
+                                            _a.trys.push([2, 6, , 7]);
+                                            a = as[i];
+                                            href = a.attribs["href"];
+                                            if (!(href !== undefined)) return [3 /*break*/, 5];
+                                            pageurl = url.resolve(me.site["url"], href);
+                                            if (!(pageurl.indexOf("javascript") < 0)) return [3 /*break*/, 5];
+                                            conf_1.Conf.procLog("site", "for: " + pageurl);
+                                            return [4 /*yield*/, pagesdb_1.Pagesdb.noPage(pageurl)];
+                                        case 3:
+                                            if (!_a.sent()) return [3 /*break*/, 5];
+                                            return [4 /*yield*/, pagesdb_1.Pagesdb.putPage(pageurl)];
+                                        case 4:
+                                            _a.sent();
+                                            _a.label = 5;
+                                        case 5: return [3 /*break*/, 7];
+                                        case 6:
+                                            e_1 = _a.sent();
+                                            // do nothing : ill url (ex. javascript)
+                                            conf_1.Conf.pdException(e_1);
+                                            return [3 /*break*/, 7];
+                                        case 7:
+                                            i++;
+                                            return [3 /*break*/, 1];
+                                        case 8:
+                                            // await result.$("a").each(async function (idx) {
+                                            //     try {
+                                            //         let pageurl: string = result.$(this).url({ invalid: false }).toString();
+                                            //         url.parse(pageurl);
+                                            //         Conf.procLog("site", "each : " + pageurl);
+                                            //         // ダウンロード済みなら実行しない
+                                            //         if(await Pagesdb.noPage(pageurl)) {
+                                            //             await Pagesdb.putPage(pageurl);
+                                            //             Conf.procLog("site", "page start : " + pageurl);
+                                            //             id++;
+                                            //             // let page = new Page(me.site["title"], pageurl, id);
+                                            //             //await page.download();
+                                            //         }
+                                            //     } catch (e) {
+                                            //         // do nothing。不正なリンクなので無視。
+                                            //     }
+                                            // });
+                                            conf_1.Conf.procLog("site", "end : for");
+                                            resolve();
+                                            return [2 /*return*/];
+                                    }
+                                });
+                            }); })];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
             });
-        });
+        }); });
     };
     return Site;
 }());
--- src/site.ts
+++ src/site.ts
@@ -12,31 +12,53 @@ export class Site {
     }
 
     download() {
-        return new Promise(resolve => {
+        return new Promise(async resolve => {
             client.set("timeout", Conf.timeout);
             let p = client.fetch(this.site["url"]);
             let me = this;
             let id = 0;
-            p.then((result: client.FetchResult) => {
-                Conf.procLog("site", "end : " + this.site.title);
-                result.$("a").each(function (idx) {
+            await p.then(async (result: client.FetchResult) => {
+                Conf.procLog("site", "dl : " + this.site.title);
+                let as = result.$("a");
+
+                for (let i = 0; i < as.length; i++) {
                     try {
-                        let pageurl: string = result.$(this).url({ invalid: false }).toString();
-                        url.parse(pageurl);
-                        Conf.procLog("site", "each : " + pageurl);
-                        // ダウンロード済みなら実行しない
-                        if(Pagesdb.noPage(pageurl)) {
-                            Pagesdb.putPage(pageurl);
-                            Conf.procLog("site", "page start : " + pageurl);
-                            id++;
-                            // let page = new Page(me.site["title"], pageurl, id);
-                            //await page.download();
+                        let a = as[i];
+                        let href = a.attribs["href"];
+                        if (href !== undefined) {
+                            let pageurl = url.resolve(me.site["url"], href);
+                            if (pageurl.indexOf("javascript") < 0) {
+                                Conf.procLog("site", "for: " + pageurl);
+                                if(await Pagesdb.noPage(pageurl)) {
+                                    await Pagesdb.putPage(pageurl);
+                                }
+                            }
                         }
                     } catch (e) {
-                        // do nothing。不正なリンクなので無視。
+                        // do nothing : ill url (ex. javascript)
+                        Conf.pdException(e);
                     }
-                });
-                Conf.procLog("site", "end : each");
+                }
+
+
+                // await result.$("a").each(async function (idx) {
+                //     try {
+                //         let pageurl: string = result.$(this).url({ invalid: false }).toString();
+                //         url.parse(pageurl);
+                //         Conf.procLog("site", "each : " + pageurl);
+                //         // ダウンロード済みなら実行しない
+                //         if(await Pagesdb.noPage(pageurl)) {
+                //             await Pagesdb.putPage(pageurl);
+                //             Conf.procLog("site", "page start : " + pageurl);
+                //             id++;
+                //             // let page = new Page(me.site["title"], pageurl, id);
+                //             //await page.download();
+                //         }
+                //     } catch (e) {
+                //         // do nothing。不正なリンクなので無視。
+                //     }
+                // });
+                Conf.procLog("site", "end : for");
                 resolve();
             });
         });
